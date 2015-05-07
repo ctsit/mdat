6e18be711bf5d00169620f24c925d1ef905498fd
Christopher Barnes <senrabc@gmail.com>
2015-05-07 15:20:18 -0400

    first commit of library for decision support. added authors and top comments

---
+++ mefdas.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env python
+
+"""mefdas.py: Description of what foobar does."""
+
+__author__      = "Philip Chase(pbc@ufl.edu, Chris Barnes(cpb@ufl.edu), Roy Keyes (keyes@ufl.edu), Alex Loiacono (atloiaco@ufl.edu)"
+__copyright__   = "Copyright 2015, CTS-IT University of Florida"
+
+
+if __name__ == "__main__":
+    import sys
+    mefdas(int(sys.argv[1]))
