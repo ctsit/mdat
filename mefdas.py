55fface07089b32a4d39d2e9a6d7deeb9ef0a23e
Philip Chase <pbc@ufl.edu>
2015-05-11 17:00:48 -0400

    Add pseudocode for set_fm_for_complex_sets and set_fm_for_trivial_cases

--- mefdas.py
+++ mefdas.py
@@ -16,12 +16,32 @@ class fuzzyMeasure:
 
     def set_fm_for_trivial_cases(self):
         # set fuzzyMeasure for empty and complete sets
+        # mu(∅) := 0
+        # mu(X) := 1
 
     def set_fm_for_singleton_sets(self):
         # set fuzzyMeasure for sets with exactly one member
 
     def set_fm_for_complex_sets(self):
         # set fuzzyMeasure for sets with 2 or more members
+
+        # Random generation of a fuzzy measure mu on a set X
+        # note: 'undefined' means we have not yet calculated and stored the value of mu for mu(foo)
+        # copy list of subsets X to my_x
+        # for each A popped randomly from my_x:
+            # if mu(A) is undefined:
+                # min := 0
+                # max := 1
+                # for each B in X:
+                    # case B ⊂ A :
+                        # if mu(B) is defined:
+                            # mu(B) = max(mu(B), min)
+                    # case B ⊃ A :
+                        # if mu(B) is defined:
+                            # mu(B) = min(max, mu(B))
+                    # else:
+                        # do nothing
+                # mu(A) := random value between min and max
 
 
 if __name__ == "__main__":
