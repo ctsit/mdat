451a3f1cb8d4d4b303f2108210e55c1584d08384
Philip Chase <pbc@ufl.edu>
2015-05-13 15:29:46 -0400

    Stub out tests for simple fuzzyMeasure methods

---
+++ test_fuzzyMeasure.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+__author__ = 'pbc'
+
+
+class TestFuzzyMeasure(TestCase):
+    def test_make_all_subsets(self):
+        self.assertEqual(1,1)
+
+    def test_set_fm_for_trivial_cases(self):
+        self.assertEqual(1,1)
+
+    def test_set_fm_for_singleton_sets(self):
+        self.assertEqual(1,1)
